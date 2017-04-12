ce9f0ec63592915023008e8fd06045837770d2f6
PedroVr <PedroVr>
2017-04-12 18:24:24 -0500

    cambios con css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-	<h1>Hola estas es mi primera vista en laravel</h1>
+	<div class="container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates illum nemo vitae molestias itaque sit eveniet soluta natus consequuntur eum labore, est dolorem mollitia reiciendis asperiores, accusamus aliquid aut, dolore.</div>
+
+	<div class="container">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias repellat, excepturi debitis quidem dolores voluptates vel molestiae voluptatibus repudiandae rem temporibus nesciunt facere libero dignissimos ullam nemo, magni. Aspernatur, quaerat?
+	</div>
 </body>
 </html>
